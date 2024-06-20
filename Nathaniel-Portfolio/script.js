aeca44bea79b971b588141ea34067749c029729a
nnathann7 <162730126+nnathann7@users.noreply.github.com>
2024-06-20 14:25:47 -0400

    Add files via upload

---
+++ Nathaniel-Portfolio/script.js
@@ -0,0 +1,30 @@
+let navLink = document.querySelector(".nav-active");
+
+if 
+
+
+
+
+
+/*
+function bigImg(x) {
+  x.style.height = "55vw";
+  x.style.width = "55vw";
+	console.log("hovering over image");
+}
+
+function normalImg(x) {
+  x.style.height = "auto";
+  x.style.width = "50vw";
+}
+
+function normalText(x) {
+  x.style.height = "auto";
+  x.style.width = "50vw";
+}
+
+function bigText(x) {
+  x.style.height = "50vw";
+  x.style.width = "50vw";
+}
+*/
